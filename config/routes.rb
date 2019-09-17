934b3fc9ae563127b9d1a28844b47ff187d6eed5
Zacahry E. Edgell <zacharyedgell@gmail.com>
2019-09-17 02:07:46 -0400

    adding format for json files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'link#index'
-  get '/top.json' to 'link#top'
+  get '/top.json', to 'link#top'
   resources :link
   get '/:short_url', to: 'link#send_to_url'
   get '/', to: 'link#index'
